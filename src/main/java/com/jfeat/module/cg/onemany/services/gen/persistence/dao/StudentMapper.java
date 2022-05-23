bedf71b42f94c14b9f37e14d9f88b90445d3d870
‘ZuoyeSir’ <1152808759@qq.com>
2022-05-23 11:07:47 +0800

    onemary

---
+++ src/main/java/com/jfeat/module/cg/onemany/services/gen/persistence/dao/StudentMapper.java
@@ -0,0 +1,8 @@
+package com.jfeat.module.cg.onemany.services.gen.persistence.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jfeat.module.cg.onemany.services.gen.persistence.model.Student;
+
+public interface StudentMapper extends BaseMapper<Student> {
+
+}
